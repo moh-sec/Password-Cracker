5a438af7fbb54c3d66e2bbd9999bf7999f651315
ЧАС <mohammadhesham93@gmail.com>
2026-01-27 15:36:12 +0200

    Update comman_passwords_check.py

--- examples/comman_passwords_check.py
+++ examples/comman_passwords_check.py
@@ -32,8 +32,6 @@ def load_common_passwords(file_path=OUTPUT_PATH):
 def check_password_strength(password):
     score = 0
 
-    if len(password) >= 8:
-        score += 1
     if len(password) >= 12:
         score += 1
 
